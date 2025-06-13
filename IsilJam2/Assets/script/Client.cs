7bd1250d6a1152296c9ea09e8e41826154d03702
Oswaldo Natanael Maguiño <156977615+dev-tective@users.noreply.github.com>
2025-06-13 14:38:53 -0500

    Create Client.cs

---
+++ IsilJam2/Assets/script/Client.cs
@@ -0,0 +1,32 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private float velocity;
+
+    public void SetVelocity(float v)
+    {
+        velocity = v;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("bullet"))
+        {
+            print("Cliente satisfecho");
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        print("Se destruyo");
+        Destroy(gameObject);
+    }
+}
